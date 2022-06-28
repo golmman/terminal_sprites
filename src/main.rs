8f578459f725151ded43b4994c36cc2693a5ae7b
Dirk Kretschmann <kretschmanndi@gmail.com>
2022-06-28 23:34:18 +0200

    print test image in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use image::GenericImageView;
+
+pub const RESET: &'static str = "\x1b[0m";
+
 fn main() {
-    println!("Hello, world!");
+    let img = image::open("tests/deer.png").unwrap();
+
+    println!("{:?}", img.dimensions());
+    println!("{:?}", img.get_pixel(10, 10).0);
+
+    let (font_w, font_h) = get_font_size();
+
+    let mut pic_width = 0;
+
+    for y in 0..img.dimensions().0 {
+
+        let mut width_actual = 0;
+
+        for x in 0..img.dimensions().1 {
+            let width_optimal = (x + 1) * font_h;
+
+            let image::Rgba([r, g, b, ..]) = img.get_pixel(x, y);
+            print!("{}", ansi_background_rgb(r, g, b));
+            while width_actual < width_optimal {
+                print!(" ");
+                width_actual += font_w;
+            }
+        }
+        println!("{}", RESET);
+    }
+    println!("{}", RESET);
+
+}
+
+fn ansi_background_rgb(r: u8, g: u8, b: u8) -> String {
+    format!("\x1b[48;2;{};{};{}m", r, g, b)
+}
+
+fn get_font_size() -> (u32, u32) {
+    (7, 17)
 }
